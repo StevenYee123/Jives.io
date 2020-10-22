93774a9e70bb48c11b3f32dcb60b6ecd67cbe8a7
Shrewbitz <swxg@umich.edu>
2020-10-22 08:46:06 -0400

    handle skills/tech

---
+++ mybot/features/handleTech.js
@@ -0,0 +1,68 @@
+const resume = require('./resume.json');
+
+module.exports = (controller) => {
+        const nextQs =  {
+            text: "Is there anything else you would like to know about me?",
+            quick_replies : [
+                {
+                    title: "Work",
+                    payload: "work"
+                },
+                {
+                    title: "About Me",
+                    payload: "about me"
+                },
+                {
+                    title: "Profiles",
+                    payload: "profiles"
+                },
+                {
+                    title: "Education",
+                    payload: "education"
+                },
+                {
+                    title: "Skills",
+                    payload: "skills"
+                },
+                {
+                    title: "No",
+                    payload: "bye"
+                }
+            ]
+        };
+
+ 
+
+    controller.hears(['tech','technologies','skills'], 'message', async(bot, message) => {
+        await bot.reply(message, {type: 'typing'});
+        
+        setTimeout(async () => {
+
+            // let info = resume.basics.email;
+            // let info = resume.skills.keywords;
+
+            // let skills = "";
+
+            // info.forEach((hash, i) => {
+            //         skills += (hash + " ")
+            // })
+
+            function displaySkills () {
+                return `test`;
+                // return skills;
+            }
+
+            await bot.changeContext(message.reference);
+            await bot.reply(message, displaySkills());
+            await bot.reply(message, {type: 'typing'});
+            setTimeout(async () => {
+               await bot.reply(message,currentJob);
+            },1000)   
+
+        }, 500);
+        await bot.reply(message, {type: 'typing'});
+        setTimeout(async () => {
+            await bot.reply(message, nextQs);
+        },1500)
+    });
+}
